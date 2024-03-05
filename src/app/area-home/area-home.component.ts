af59af7c91163aa2a88a84e39a30b14001878a19
MisterL-v <leonardvoss0@gmail.com>
2024-03-05 17:02:05 +0100

    Additions to home

--- src/app/area-home/area-home.component.ts
+++ src/app/area-home/area-home.component.ts
@@ -1,5 +1,8 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {NgIf} from "@angular/common";
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {interval, Subscription} from "rxjs";
+
 
 @Component({
   selector: 'app-area-home',
@@ -17,4 +20,15 @@ export class AreaHomeComponent{
 
   // Get data from parent (surface component)
   @Input() system_language : string = '';
+
+  subscriptionTimer : Subscription;
+  // Intervall to update the current time, in milliseconds
+  timer = 60000;
+
+  constructor(private http: HttpClient) {
+    // Timer to update the current time in a regular interval, defined by the timer variable
+    this.subscriptionTimer = interval(this.timer).subscribe(x =>{this.updateTime()});
+  }
+
+
 }
